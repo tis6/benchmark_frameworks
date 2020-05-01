1ce4170eeb9233dc380c069da704454c8051f242
Rafael Badaró <rafabadaro.15@gmail.com>
2020-05-01 18:51:05 -0300

    Criando csv de infos gerais e de cada issue

--- script_graphql.py
+++ script_graphql.py
@@ -4,6 +4,7 @@ import csv
 
 headers = {"Authorization": "token ###"}
 
+
 class Repository:
     def __init__(self, owner, name, merged_pull_requests, created_at, total_releases, total_closed_issues, closed_issues):
         self.owner = owner
@@ -13,6 +14,7 @@ class Repository:
         self.total_releases = total_releases
         self.total_closed_issues = total_closed_issues
         self.closed_issues = closed_issues
+
 
 class Issue:
     def __init__(self, number, title, created_at, closed_at):
@@ -35,11 +37,13 @@ def run_query(query):
         raise Exception("Query failed to run by returning code of {}. {}".format(
             request.status_code, query))
 
-#Minera os repositórios
+# Minera os repositórios
+
+
 def mine(owner, name):
   # 1) Buscar os dados base que não precisam de loop
   # TODO - Mudar pra pegar independente de repos
-  queryBase = """ 
+  queryBase = """
     {
     repository(owner: "%s", name: "%s") {
         mergedPullRequests: pullRequests(states: MERGED) {
@@ -56,19 +60,19 @@ def mine(owner, name):
     }""" % (owner, name)
 
   queryResultBase = run_query(queryBase)
- 
+
   total_merged_pull_requests = queryResultBase['data']['repository']['mergedPullRequests']['totalCount']
   created_at = queryResultBase['data']['repository']['createdAt']
-  total_releases =   queryResultBase['data']['repository']['releases']['totalCount']
-  total_closed_issues =  queryResultBase['data']['repository']['totalIssuesFechada']['totalCount']
+  total_releases = queryResultBase['data']['repository']['releases']['totalCount']
+  total_closed_issues = queryResultBase['data']['repository']['totalIssuesFechada']['totalCount']
 
-  repo = Repository(owner, name, total_merged_pull_requests, created_at, total_releases, total_closed_issues, [])
- 
+  repo = Repository(owner, name, total_merged_pull_requests,
+                    created_at, total_releases, total_closed_issues, [])
 
   # 2) Buscar as issues do repository (loop)
 
   endCursor = "null"  # Proxima pagina
-  closed_issues = [] 
+  closed_issues = []
 
   interval = total_closed_issues//50
   print('Iniciando buscas das issues, numero de repeticoes:' + str(interval))
@@ -96,7 +100,8 @@ def mine(owner, name):
     queryResultIssue = run_query(queryIssue)
     querySize = len(queryResultIssue['data']['repository']['issues']['nodes'])
     # Pega o endCursor aka proxima pagina
-    endCursor = '"{}"'.format(queryResultIssue['data']['repository']['issues']['pageInfo']['endCursor'])
+    endCursor = '"{}"'.format(
+        queryResultIssue['data']['repository']['issues']['pageInfo']['endCursor'])
 
     # Monta e adiciona o obj de issue em uma lista
     for y in range(querySize):
@@ -109,59 +114,70 @@ def mine(owner, name):
 
           # Salva os nodes no array de nodes
           closed_issues.append(new_issue_from_query)
-    
-    if x%10 == 0:
+
+    if x % 10 == 0:
       print('Loop:' + str(x))
-  
 
   repo.closed_issues = closed_issues
   return repo
 
 # Escreve em um arquivo csv
-def writeCsv(nodes):
-  with open("/Users/Rafael/Desktop/labex/repos.csv", 'w') as new_file:
+
+
+def writeCsv(repo, name):
+  file_infos = "/Users/Rafael/Desktop/benchmark_frameworks/csv_github/repos_graphql_%s.csv" % name
+  with open(file_infos, 'w', encoding="utf-8") as new_file_info:
 
     fnames = [
-        'name_with_owner',
-        'url',
+        'owner',
+        'name',
         'created_at',
-        'updated_at',
         'merged_pull_requests',
-        'releases',
-        'primary_language',
-        'total_issues',
-        'total_issues_closed']
+        'total_releases',
+        'total_closed_issues']
 
-    csv_writer = csv.DictWriter(new_file, fieldnames=fnames)
+    csv_writer = csv.DictWriter(new_file_info, fieldnames=fnames)
     csv_writer.writeheader()
-    for node in nodes:
-        csv_writer.writerow(
+    csv_writer.writerow(
             {
-                'name_with_owner': node['nameWithOwner'],
-                'url': node['url'],
-                'created_at': node['createdAt'],
-                'updated_at': node['updatedAt'],
-                'merged_pull_requests': node['pullRequests']['totalCount'],
-                'releases': node['releases']['totalCount'],
-                'primary_language': node['primaryLanguage']['name'] if node['primaryLanguage']!= None else 'null',
-                'total_issues': node['numeroTotalIssues']['totalCount'],
-                'total_issues_closed': node['numeroTotalIssuesClosed']['totalCount'],
+                'owner': repo.owner,
+                'name': repo.name,
+                'created_at': repo.created_at,
+                'merged_pull_requests': repo.merged_pull_requests,
+                'total_releases': repo.total_releases,
+                'total_closed_issues': repo.total_closed_issues
             })
 
-    print('Arquivo csv gerado com sucesso!')
+    print('Arquivo csv infos gerado com sucesso!')
+
+  print('Iniciando geração do csv de issues')
+  file_issues = "/Users/Rafael/Desktop/benchmark_frameworks/csv_github/repos_graphql_%s_issues.csv" % name
+  with open(file_issues, 'w', encoding="utf-8") as new_file_issues:
+        fnames = [
+                'number',
+                'title',
+                'created_at',
+                'closed_at']
+
+        csv_writer = csv.DictWriter(new_file_issues, fieldnames=fnames)
+        csv_writer.writeheader()
+
+        for issue in repo.closed_issues:
+             csv_writer.writerow(
+                    {
+                        'number': issue.number,
+                        'title': issue.title,
+                        'created_at': issue.created_at,
+                        'closed_at': issue.closed_at,
+                    })
+
+        print('Arquivo csv issues gerado com sucesso!')
+
+owners = ['expressjs', 'gin-gonic', 'django', 'rails', 'playframework', 'ktorio', 'dotnet', 'spring-projects', 'vapor', 'laravel']
+names = ['express', 'gin', 'django', 'rails', 'playframework', 'ktor', 'core', 'spring-framework', 'vapor', 'laravel']
 
 
-repo = mine("expressjs", "express")
-print(repo.owner)
-print(repo.name)
-print(repo.merged_pull_requests)
-print(repo.created_at)
-print(repo.total_releases)
-print(repo.total_closed_issues)
 
-print(repo.closed_issues[0].number)
-print(repo.closed_issues[0].title)
-print(repo.closed_issues[0].created_at)
-print(repo.closed_issues[0].closed_at)
-
-# writeCsv(nodes)
+for x in range(10):
+    repo = mine(owners[x], names[x])
+    writeCsv(repo, names[x])
